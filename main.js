6becab011ccb459add3dadd85d8ec43961be9442
matyyng98 <matiasgallardo1998@gmail.com>
2023-01-30 21:16:01 -0300

    1.0.0

---
+++ main.js
@@ -0,0 +1,30 @@
+//Para que el menú aparezca cuando se achica la pantalla
+
+const burger = document.querySelector("#menu-hamburguesa");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+//Para que el menú se cierre cuando se hace click en alguna opción del mismo
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// para desplazarse hacia arriba
+const desplazarArriba = document.querySelector("#desplazarse-hacia-arriba");
+
+desplazarArriba.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
